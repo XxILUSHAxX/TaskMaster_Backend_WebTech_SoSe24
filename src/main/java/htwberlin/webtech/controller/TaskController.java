1a929ab89e557dbb9b843fedfec38851dc9e024e
Ilie Chicioroaga <106095042+XxILUSHAxX@users.noreply.github.com>
2024-07-02 09:02:27 +0200

    doku-text

--- src/main/java/htwberlin/webtech/controller/TaskController.java
+++ src/main/java/htwberlin/webtech/controller/TaskController.java
@@ -7,8 +7,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
+/**
+ * @author ILUSA
+ */
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
